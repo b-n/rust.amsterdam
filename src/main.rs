31d7ff0ca688180e1c4c49299ad38a976a5970de
Ben Naylor <nayben@gmail.com>
2023-10-29 10:23:31 +0100

    Restyle and generate build pipeline for the website

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 use clap::{command, Parser, Subcommand};
 use inviteify::{ChannelInviteRequest, Inviteify};
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
+
+fn default_build_path() -> PathBuf {
+    let mut p = env::current_dir().expect("Unable to locate current working directory");
+    p.push("build");
+    p
+}
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,6 +40,8 @@ enum Commands {
     Build {
         #[arg(long, env = "DISCORD_CHANNEL_ID")]
         channel_id: String,
+        #[arg(long, default_value = default_build_path().into_os_string())]
+        output_dir: PathBuf,
     },
     BotInviteLink {
         #[arg(long, env = "DISCORD_SERVER_ID")]
@@ -25,12 +49,26 @@ enum Commands {
     },
 }
 
+#[derive(Debug)]
+struct BuildError(String);
+
+impl std::error::Error for BuildError {}
+
+impl std::fmt::Display for BuildError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "BuildError: {}", self.0)
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     match &args.command {
-        Commands::Build { channel_id } => build(&args.token, &args.client_id, channel_id).await,
+        Commands::Build {
+            channel_id,
+            output_dir,
+        } => build(&args.token, &args.client_id, channel_id, output_dir).await,
         Commands::BotInviteLink { server_id } => {
             bot_invite_link(&args.token, &args.client_id, server_id).await
         }
@@ -41,24 +79,40 @@ async fn build(
     token: &str,
     client_id: &str,
     channel_id: &str,
+    output_dir: &Path,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let _service = Inviteify::new(client_id, token)?;
+    let mut service = Inviteify::new(client_id, token)?;
 
-    // Clean/create build folder
-    // Recursive copy static to build
-    // Generate QR invite link
+    // Clean build dir and create anew
+    if output_dir.exists() {
+        println!("Cleaning output directory");
+        fs::remove_dir_all(output_dir)
+            .map_err(|_| BuildError("Failed to clean build directory".to_string()))?;
+    }
+    fs::create_dir(output_dir)?;
 
-    //let req = ChannelInviteRequest {
-    //max_age: 86400,
-    //..ChannelInviteRequest::default()
-    //};
+    // Generate QR Code
+    let req = ChannelInviteRequest {
+        max_age: 86400,
+        ..ChannelInviteRequest::default()
+    };
 
-    //let invite_code = service
-    //.check_and_generate_invite(&discord_channel_id, &req)
-    //.await?;
+    let invite = service.check_and_generate_invite(channel_id, &req).await?;
 
-    println!("Output generated in build/");
+    let qrcode = service
+        .invite_qrcode(&invite)?
+        .render::<qrcode::render::svg::Color>()
+        .min_dimensions(200, 200)
+        .build();
+    fs::write(output_dir.join("discord_invite.svg"), qrcode)?;
 
+    // Recursive copy static/ to build output
+    let static_path = env::current_dir().unwrap().join("static");
+    if static_path.is_dir() {
+        copy_dir_all(&static_path, output_dir)?
+    }
+
+    println!("Build generated in {}", output_dir.display());
     Ok(())
 }
 
@@ -73,5 +127,6 @@ async fn bot_invite_link(
         "Click the following link and follow the prompts to authorize the bot:\n{}",
         service.authorization_link(server_id)
     );
+
     Ok(())
 }
